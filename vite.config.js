bd55a09ec1d7769217545811dc4bffcf268c9334
VoidX3D <playzspreston2@gmail.com>
2025-12-25 10:53:48 +0545

    comit message template

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite';
-import { ViteFaviconsPlugin } from 'vite-plugin-favicon';
+
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    [react()],
-    ViteFaviconsPlugin('./public/favicon.svg') // or use SVG
-  ]
+  plugins: [react()]
 })
